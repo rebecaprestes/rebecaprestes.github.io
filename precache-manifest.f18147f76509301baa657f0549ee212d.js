58cba85eda212f4af1fd9ce3d011da88a67e49e8
Rebeca Prestes <rebecaprestes@live.com>
2019-01-23 13:05:58 +0000

    Updates

---
+++ precache-manifest.f18147f76509301baa657f0549ee212d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8269d3871d020efd837587043df0e1c",
+    "url": "/public/static/media/nutti.c8269d38.png"
+  },
+  {
+    "revision": "977d4b2dac9cdf7f059b",
+    "url": "/public/static/css/main.dedf8462.chunk.css"
+  },
+  {
+    "revision": "e72f9b5b4fce1fab0661",
+    "url": "/public/static/js/1.e72f9b5b.chunk.js"
+  },
+  {
+    "revision": "c3ea8d972d62e65a19a9",
+    "url": "/public/static/js/runtime~main.c3ea8d97.js"
+  },
+  {
+    "revision": "953c18492e6cca5144ca1e018be6a9c4",
+    "url": "/public/static/media/css3.953c1849.png"
+  },
+  {
+    "revision": "1a7592f68c239a4d32af65854b3f9a9b",
+    "url": "/public/static/media/wordpress.1a7592f6.png"
+  },
+  {
+    "revision": "4b651955038bda12fd3d9fd4160f7369",
+    "url": "/public/static/media/reactjs.4b651955.jpg"
+  },
+  {
+    "revision": "977d4b2dac9cdf7f059b",
+    "url": "/public/static/js/main.977d4b2d.chunk.js"
+  },
+  {
+    "revision": "4675091d5048555244711edad1942e6d",
+    "url": "/public/static/media/useconclave2.4675091d.png"
+  },
+  {
+    "revision": "33b0a7d4c3214367c9df9e7797403e04",
+    "url": "/public/static/media/blog-useconclave.33b0a7d4.png"
+  },
+  {
+    "revision": "0fa7990c1b8cabb3b5f1b7b12dd3a497",
+    "url": "/public/static/media/jammies.0fa7990c.png"
+  },
+  {
+    "revision": "8301f08c25560a7de07b40036e1060ed",
+    "url": "/public/static/media/vondutch.8301f08c.png"
+  },
+  {
+    "revision": "784dbc0e57cdecfc7862bdd89f11c438",
+    "url": "/public/static/media/author-hp.784dbc0e.png"
+  },
+  {
+    "revision": "c45a1ee5c49fedc025caeee1849bfe3d",
+    "url": "/public/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94bc1f788b96a4d8b8af8f3a01e2f24b.js"
+  "/public/precache-manifest.f18147f76509301baa657f0549ee212d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/public/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
